6bc59988d89631fd0950ce3c4bb74473d3bc2561
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-04-03 14:30:21 +0100

    replace static and image cache with single asset-cache

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,42 +27,18 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // Implemented asset caching
-
-const cacheName = 'static-resources';
-const matchCallback = ({ request }) => {
-  return (
-    // CSS
-    request.destination === 'style' ||
-    // JavaScript
-    request.destination === 'script'
-  );
-};
-
+// Set up asset cache
 registerRoute(
-  matchCallback,
+  // Here defined the callback function that will filter the requests we want to cache (in this case, JS, CSS files and Images)
+  ({ request }) =>
+    ['style', 'script', 'worker', 'image'].includes(request.destination),
   new StaleWhileRevalidate({
-    cacheName,
+    // Name of the cache storage.
+    cacheName: 'asset-cache',
     plugins: [
+      // This plugin will cache responses with these headers to a maximum-age of 30 days
       new CacheableResponsePlugin({
         statuses: [0, 200],
-      }),
-    ],
-  })
-);
-
-// Register route for caching images
-// The cache first strategy is often the best choice for images because it saves bandwidth and improves performance.
-registerRoute(
-  ({ request }) => request.destination === 'image',
-  new CacheFirst({
-    cacheName: 'my-image-cache',
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-      new ExpirationPlugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
       }),
     ],
   })
